b0b3006af82bb92397bbba93a204b995e39b5b69
AbigailMon <MonfortoAS28@uww.edu>
2024-04-03 10:37:35 -0500

    Fibonacci code

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+# This is a python program to calculate the fibonacci sequence. Create the fibonacci sequence using a generator.
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+n = int(input("Enter the number of terms: "))
+for i in range(n):
+    print(fib(i), end=" ")
